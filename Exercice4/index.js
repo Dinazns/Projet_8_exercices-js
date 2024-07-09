3eb683870f2a7e7b40f2a06aa3fd6df567b31427
Dinazns <dinaz2000@hotmail.fr>
2024-07-09 17:17:00 +0200

    Exercice 4 fait

---
+++ Exercice4/index.js
@@ -0,0 +1,7 @@
+const myButton = document.getElementById("myButton");
+
+myButton.addEventListener("click", function () {
+    const TextButton = document.getElementById("TextClickButton");
+    TextButton.textContent = "Bonjour, vous avez cliqué sur le bouton !";
+});
+
