e21947afb7dc97c1dfbab151f639cc628a18b787
Dinazns <dinaz2000@hotmail.fr>
2024-07-09 16:54:26 +0200

    Exercice 3 fait

--- Exercice3/index.js
+++ Exercice3/index.js
@@ -1,5 +1,23 @@
 // Créez votre fonction ici
 
+function calculateAverage (array) {
+
+    if (!Array.isArray(array) || array.length === 0 ) {
+        return "No numbers to calculate average";
+    }
+
+    let sum = 0;
+    let moyenne;
+
+    array.forEach(element => {
+        sum += element;
+    });
+
+    moyenne = sum / array.length;
+
+    return moyenne;
+}
+
 // Exemples d'utilisation de la fonction
 console.log(calculateAverage([5, 10, 15])) // retourne 10
 console.log(calculateAverage([10, 20, 30, 20])) // retourne 20
